0d58441beaa424bca60431ba4f1d020695d1fe76
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-04-27 17:59:21 -0500

    removed conditional logic

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -25,9 +25,7 @@ mkdir -p $DEST_DIR
 ## put the yaml resource content in DEST_DIR
 cp -R "${CHART_DIR}"/* "${DEST_DIR}"
 
-if [[ -n "${VALUES_CONTENT}" ]]; then
-  echo "${VALUES_CONTENT}" > "${DEST_DIR}/values.yaml"
-fi
+echo "${VALUES_CONTENT}" > "${DEST_DIR}/values.yaml"
 
 cat "${DEST_DIR}/values.yaml"
 
