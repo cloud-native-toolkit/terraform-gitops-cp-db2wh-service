788cfb81e66ca3996d53b6421b60414a6bbb92c0
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-04-27 13:18:11 -0500

    Debugging stmt

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -6,6 +6,18 @@ MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
 NAME="$1"
 DEST_DIR="$2"
 
+echo "Name Print"
+echo "${NAME}"
+
+echo "SCRIPT_DIR"
+echo "${SCRIPT_DIR}"
+
+echo "MODULE_DIR"
+echo "${MODULE_DIR}"
+
+echo "DEST_DIR"
+echo "${DEST_DIR}"
+
 mkdir -p $DEST_DIR
 
 ## Add logic here to put the yaml resource content in DEST_DIR
