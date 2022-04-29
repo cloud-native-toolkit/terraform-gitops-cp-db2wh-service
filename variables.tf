847d9d1c63f5a9ed420d932a2b8f34094054cc98
balasgit <bala@us.ibm.com>
2022-04-29 07:29:49 -0500

    Added namespace

--- variables.tf
+++ variables.tf
@@ -65,6 +65,7 @@ variable "git_credentials" {
 variable "namespace" {
   type        = string
   description = "The namespace where the application should be deployed"
+  default ="cpd-operators"
 }
 
 variable "cluster_ingress_hostname" {
