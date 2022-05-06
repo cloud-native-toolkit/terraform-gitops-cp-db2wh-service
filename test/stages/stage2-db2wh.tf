a9450a779fc04070c410905624d4dfeb905343ac
balasgit <bala@us.ibm.com>
2022-05-06 16:03:58 -0500

    fixed the namespace name

--- test/stages/stage2-db2wh.tf
+++ test/stages/stage2-db2wh.tf
@@ -4,7 +4,7 @@ module "db2wh" {
   gitops_config            = module.gitops.gitops_config
   git_credentials          = module.gitops.git_credentials
   server_name              = module.gitops.server_name
-  namespace                = module.gitops.namespace
+  namespace                = module.gitops_namespace.name
   kubeseal_cert            = module.gitops.sealed_secrets_cert
 
   operator_namespace= "cpd-operators"  
