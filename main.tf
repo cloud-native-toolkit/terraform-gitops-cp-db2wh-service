36495fc5312ae1f24273c0187b6aad9c7e797754
balasgit <bala@us.ibm.com>
2022-05-12 13:40:39 -0500

    Added db2wh instance

--- main.tf
+++ main.tf
@@ -145,3 +145,47 @@ resource null_resource setup_gitops_subscription {
   }
 }
 
+resource null_resource create_instance_yaml {
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/create-yaml.sh '${local.name}' '${local.instance_yaml_dir}'"
+
+    environment = {
+      VALUES_CONTENT = yamlencode(local.instance_content)
+    }
+  }
+}
+
+resource null_resource setup_gitops_instance {
+  depends_on = [null_resource.create_instance_yaml]
+
+  triggers = {
+    name = local.name
+    namespace = var.namespace
+    yaml_dir = local.instance_yaml_dir
+    server_name = var.server_name
+    layer = local.layer
+    type = local.type
+    git_credentials = yamlencode(var.git_credentials)
+    gitops_config   = yamlencode(var.gitops_config)
+    bin_dir = local.bin_dir
+  }
+
+  provisioner "local-exec" {
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
+}
