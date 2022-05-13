090dcf577f8744c37914e0a4b67d85e3c8696ce7
balasgit <bala@us.ibm.com>
2022-05-12 19:21:12 -0500

    Added instance condition

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -72,14 +72,14 @@ echo "CPD_NAMESPACE *****"${CPD_NAMESPACE}""
 sleep 60
 INSTANCE_STATUS=""
 
-#  while [ true ]; do
-#    INSTANCE_STATUS=$(kubectl get Db2whService db2wh-cr -n "${CPD_NAMESPACE}" -o jsonpath='{.status.db2whStatus} {"\n"}')
-#    echo "Waiting for instance "${INSTANCE_NAME}" to be ready. Current status : "${INSTANCE_STATUS}""
-#    if [ $INSTANCE_STATUS == "Completed" ]; then
-#      break
-#    fi
-#    sleep 30
-#  done
+  while [ true ]; do
+    INSTANCE_STATUS=$(kubectl get Db2whService db2wh-cr -n "${CPD_NAMESPACE}" -o jsonpath='{.status.db2whStatus} {"\n"}')
+    echo "Waiting for instance "${INSTANCE_NAME}" to be ready. Current status : "${INSTANCE_STATUS}""
+    if [ $INSTANCE_STATUS == "Completed" ]; then
+      break
+    fi
+    sleep 30
+  done
 
 echo "DB2 Db2whService/db2wh-cr is ${INSTANCE_STATUS}"
 
