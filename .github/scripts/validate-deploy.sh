35b71fa50a752b15428924c159e2811b1da0246b
balasgit <bala@us.ibm.com>
2022-05-17 18:29:45 -0500

    added variable namespace

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -92,11 +92,11 @@ echo "DB2 Operator uninstall"
 
 #oc get Db2whService -n project-name
 
-#oc delete Db2whService db2wh-cr -n ${CPD_NAMESPACE}
-
 #oc delete csv ${CSV} -n ${OPERATOR_NAMESPACE}
 
-oc delete csv ibm-db2wh-cp4d-operator.v1.0.10 -n cpd-operators
+oc delete csv ${SUBSCRIPTION_NAME} -n ${OPERATOR_NAMESPACE}
+
+oc delete Db2whService db2wh-cr -n ${CPD_NAMESPACE}
 
 cd ..
 rm -rf .testrepo
