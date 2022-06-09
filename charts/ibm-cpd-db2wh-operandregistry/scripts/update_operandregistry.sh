1ebfe41e61b91c7bbdbdbecca9039745f3a6ee72
balasgit <bala@us.ibm.com>
2022-06-08 21:01:17 -0500

    updated operand request variables

--- charts/ibm-cpd-db2wh-operandregistry/scripts/update_operandregistry.sh
+++ charts/ibm-cpd-db2wh-operandregistry/scripts/update_operandregistry.sh
@@ -10,6 +10,9 @@ SOURCE_NAME="ibm-db2uoperator-catalog"
 echo "ZEN Operator namespace"
 echo $ZEN_OPERATORS_NAMESPACE
 
+echo "COMMON Operator namespace"
+echo $COMMON_SERVICES_NAMESPACE
+
 oc get operandregistry common-service -n ${COMMON_SERVICES_NAMESPACE} -o json > /temp/operandregistry.json
 
 jq --arg ZEN_OPERATORS_NAMESPACE $ZEN_OPERATORS_NAMESPACE '(.spec.operators[] | select(.name == "ibm-db2u-operator")).namespace |= $ZEN_OPERATORS_NAMESPACE' /temp/operandregistry.json > /temp/operandregistry_new.json
